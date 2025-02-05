f69b82b9ee494582946e2b1f7bcbf9e3a3d73f5e
Sam Minot <sminot@gmail.com>
2025-02-05 15:02:05 -0800

    Use the Cirro patch for pyodide

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import marimo
 
 __generated_with = "0.11.0"
-app = marimo.App(
-    width="medium",
-    app_title="Differential Expression Viewer"
-)
+app = marimo.App(width="medium", app_title="Differential Expression Viewer")
 
 
 @app.cell
@@ -15,47 +12,45 @@ def _(mo):
 
 @app.cell
 def _():
+    # Load the marimo library in a dedicated cell for efficiency
     import marimo as mo
     return (mo,)
 
 
 @app.cell
 def _():
-    import sys
-
     # If the script is running in WASM (instead of local development mode), load micropip
+    import sys
     if "pyodide" in sys.modules:
         import micropip
+        running_in_wasm = True
     else:
         micropip = None
-    return micropip, sys
+        running_in_wasm = False
+    return micropip, running_in_wasm, sys
 
 
 @app.cell
-async def _(micropip, mo):
+async def _(cirro, micropip, mo, running_in_wasm):
     with mo.status.spinner("Loading dependencies"):
-        if micropip is not None:
+        # If we are running in WASM, some dependencies need to be set up appropriately.
+        # This is really just aligning the needs of the app with the default library versions
+        # that come when a marimo app loads in WASM.
+        if running_in_wasm:
             print("Installing via micropip")
+            # Downgrade plotly to avoid the use of narwhals
             await micropip.install("plotly<6.0.0")
             await micropip.install("ssl")
+            micropip.uninstall("urllib3")
+            micropip.uninstall("httpx")
             await micropip.install(["urllib3==2.3.0"])
-            print('added urllib3==2.3.0')
             await micropip.install([
                 "boto3==1.36.3",
                 "botocore==1.36.3"
             ], verbose=True)
-            print('added boto 1.36.3')
-            await micropip.install(["cirro>=1.2.15"], verbose=True)
-            print('added cirro')
-        from cirro import DataPortalLogin
-        from cirro.config import list_tenants
-    return DataPortalLogin, list_tenants
+            await micropip.install(["cirro[pyodide]>=1.2.16"], verbose=True)
 
-
-@app.cell
-def _(mo):
-    with mo.status.spinner("Loading dependencies"):
-        from io import StringIO
+        from io import StringIO, BytesIO
         from queue import Queue
         from time import sleep
         from typing import Dict, Optional
@@ -63,16 +58,30 @@ def _(mo):
         import pandas as pd
         import numpy as np
         from functools import lru_cache
-        from io import BytesIO
         import base64
         from urllib.parse import quote_plus
+
+        from cirro import DataPortalLogin
+        from cirro.services.file import FileService
+        from cirro.sdk.file import DataPortalFile
+        from cirro.config import list_tenants
+
+        # A patch to the Cirro client library is applied when running in WASM
+        if running_in_wasm:
+            from cirro.helpers import pyodide_patch_all
+            pyodide_patch_all()
+
     return (
         BytesIO,
+        DataPortalFile,
+        DataPortalLogin,
         Dict,
+        FileService,
         Optional,
         Queue,
         StringIO,
         base64,
+        list_tenants,
         lru_cache,
         np,
         pd,
@@ -188,7 +197,10 @@ def _(data_source_ui, domain_to_name, mo, query_params, tenants_by_name):
 
 @app.cell
 def _(DataPortalLogin, domain_ui, get_client, mo):
-    # If the user is not yet logged in, and a domain is selected
+    # If the user is not yet logged in, and a domain is selected, then give the user instructions for logging in
+    # The configuration of this cell and the two below it serve the function of:
+    #   1. Showing the user the login instructions if they have selected a Cirro domain
+    #   2. Removing the login instructions as soon as they have completed the login flow
     if get_client() is None and domain_ui.value is not None:
         with mo.status.spinner("Authenticating"):
             # Use device code authorization to log in to Cirro
@@ -246,7 +258,7 @@ def _(client):
 @app.cell
 def _(id_to_name, mo, name_to_id, projects, query_params):
     # Let the user select which project to get data from
-    project_ui=mo.ui.dropdown(
+    project_ui = mo.ui.dropdown(
         value=id_to_name(projects, query_params.get("project")),
         options=name_to_id(projects),
         on_change=lambda i: query_params.set("project", i)
@@ -257,9 +269,10 @@ def _(id_to_name, mo, name_to_id, projects, query_params):
 
 @app.cell
 def _(client, mo, project_ui):
-    # Get the list of datasets available to the user
+    # Stop if the user has not selected a project
     mo.stop(project_ui.value is None)
 
+    # Get the list of datasets available to the user
     # Filter the list of datasets by type (process_id)
     datasets = [
         dataset
@@ -277,7 +290,7 @@ def _(client, mo, project_ui):
 @app.cell
 def _(datasets, id_to_name, mo, name_to_id, query_params):
     # Let the user select which dataset to get data from
-    dataset_ui=mo.ui.dropdown(
+    dataset_ui = mo.ui.dropdown(
         value=id_to_name(datasets, query_params.get("dataset")),
         options=name_to_id(datasets),
         on_change=lambda i: query_params.set("dataset", i)
@@ -288,9 +301,10 @@ def _(datasets, id_to_name, mo, name_to_id, query_params):
 
 @app.cell
 def _(client, dataset_ui, mo, project_ui):
-    # Get the list of files within the selected dataset
+    # Stop if the user has not selected a dataset
     mo.stop(dataset_ui.value is None)
 
+    # Get the list of files within the selected dataset
     file_list = [
         file.name
         for file in (
@@ -306,7 +320,7 @@ def _(client, dataset_ui, mo, project_ui):
 @app.cell
 def _(file_list, mo, query_params):
     # Let the user select which file to get data from
-    file_ui=mo.ui.dropdown(
+    file_ui = mo.ui.dropdown(
         value=(query_params.get("file") if query_params.get("file") in file_list else None),
         options=file_list,
         on_change=lambda i: query_params.set("file", i)
@@ -329,17 +343,19 @@ def _(mo, query_params):
 
 @app.cell
 def _(client, dataset_ui, file_ui, project_ui, sep_ui, set_df):
-    # Here is where we read in the table as a DataFrame from Cirro
-
     # If the file was selected
     if file_ui.value is not None:
+        # Read the table and set the state
         set_df(
-            client
-            .get_project_by_id(project_ui.value)
-            .get_dataset_by_id(dataset_ui.value)
-            .list_files()
-            .get_by_id(file_ui.value)
-            .read_csv(sep=dict(comma=",", tab="\t", space=" ")[sep_ui.value])
+            (
+                client
+                .get_project_by_id(project_ui.value)
+                .get_dataset_by_id(dataset_ui.value)
+                .list_files()
+                .get_by_id(file_ui.value)
+                # Set the delimiter used to read the file based on the menu selection
+                .read_csv(sep=dict(comma=",", tab="\t", space=" ")[sep_ui.value])
+            )
         )
     return
 
